23252c62d3722e10b1455d7f01de3e1cdd0136f2
StoneTrench <rlgamer04@gmail.com>
2022-10-17 15:23:21 +0200

    Minor fixes

--- index.ts
+++ index.ts
@@ -1,52 +1,5 @@
 import { Bot } from "mineflayer";
 
-declare module "mineflayer" {
-    interface Bot {
-        taskManager: {
-            /**
-             * Add an action to the task queue.
-             * @param name The name of the action use it to distinguish it from the rest.
-             * @param action the promise based function to execute when we get to it.
-             * @param delay the time in ms to wait before executing the action, set to 0 by default.
-             */
-            Add: (name: string, action: (bot: Bot) => (Promise<any> | void), delay?: number) => void;
-            /**
-             * Add an action to the start of the task queue.
-             * @param name The name of the action use it to distinguish it from the rest.
-             * @param action the promise based function to execute when we get to it.
-             * @param delay the time in ms to wait before executing the action, set to 0 by default.
-             */
-            Insert: (name: string, action: (bot: Bot) => (Promise<any> | void), delay?: number) => void;
-            /**
-             * Remove an action from the queue.
-             * @param name The name of the action use it to distinguish it from the rest.
-             */
-            Remove: (name: string) => void;
-            /**
-             * Get an action from the queue.
-             * @param index the index of the task, set to 0 by default.
-             */
-            Get: (index?: number) => BotTask;
-            /**
-             * Get the queue.
-             */
-            GetWholeQueue: () => BotTask[];
-            /**
-             * Removes every element from the queue.
-             */
-            Clear: () => void;
-            /**
-             * Stops executing tasks in the queue.
-             */
-            Pause: () => void;
-            /**
-             * Resumes executing tasks in the queue.
-             */
-            Resume: () => void;
-        }
-    }
-}
-
 class BotTask {
     name: string;
     action: (bot: Bot) => Promise<any>;
